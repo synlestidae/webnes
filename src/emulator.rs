fc4491080be2baa98e8d95f954614c7ecc370156
Maté Antunovic <just.mate.antunovic@gmail.com>
2019-01-02 14:11:17 +1300

    Make emulator.rs compile

--- src/emulator.rs
+++ src/emulator.rs
@@ -1,56 +1,95 @@
 use mem::*;
 use rom::Rom;
-use cpu::Cpu;
+use cpu::{Cpu};
+use gfx::*;
 use input_source::*;
+use input::*;
 
-pub struct Emulator<M: Mem> {
+pub struct Emulator<'a> {
     rom: Box<Rom>,
-    memmap: MemMap,
-    Cpu: Cpu<M>,
+    //memmap: MemMap,
+    cpu: Cpu<MemMap>,
+    gfx: Gfx<'a>,
     last_time: f64,
     frames: usize
 }
 
-impl<M: Mem> Emulator<M> {
-    pub fn new() -> Self {
-        unimplemented!()
+impl<'a> Emulator<'a> {
+    pub fn new(mem: MemMap, rom: Box<Rom>, gfx: Gfx<'a>) -> Self {
+        Self {
+            rom: rom,
+            cpu: Cpu::new(mem),
+            gfx: gfx,
+            last_time: 0.0,
+            frames: 0
+        }
     }
 
     pub fn step(&mut self) {
-        cpu.step();
+        self.cpu.step();
 
-        let ppu_result = cpu.mem.ppu.step(cpu.cy);
+        let ppu_result = self.cpu.mem.ppu.step(self.cpu.cy);
         if ppu_result.vblank_nmi {
-            cpu.nmi();
+            self.cpu.nmi();
         } else if ppu_result.scanline_irq {
-            cpu.irq();
+            self.cpu.irq();
         }
 
-        cpu.mem.apu.step(cpu.cy);
+        self.cpu.mem.apu.step(self.cpu.cy);
 
         if ppu_result.new_frame {
-            gfx.tick();
-            gfx.composite(&mut *cpu.mem.ppu.screen);
-            record_fps(&mut last_time, &mut frames);
-            cpu.mem.apu.play_channels();
+            self.gfx.tick();
+            self.gfx.composite(&mut *self.cpu.mem.ppu.screen);
+            record_fps(&mut self.last_time, &mut self.frames);
+            self.cpu.mem.apu.play_channels();
 
-            match cpu.mem.input.check_input() {
+            match self.cpu.mem.input.check_input() {
                 InputResult::Continue => {}
-                InputResult::Quit => break,
+                InputResult::Quit => {},
                 InputResult::SaveState => {
-                    cpu.save(&mut File::create(&Path::new("state.sav")).unwrap());
-                    gfx.status_line.set("Saved state".to_string());
+                    //self.cpu.save(&mut File::create(&Path::new("state.sav")).unwrap());
+                    self.gfx.status_line.set("Saved state".to_string());
                 }
                 InputResult::LoadState => {
-                    cpu.load(&mut File::open(&Path::new("state.sav")).unwrap());
-                    gfx.status_line.set("Loaded state".to_string());
+                    //self.cpu.load(&mut File::open(&Path::new("state.sav")).unwrap());
+                    self.gfx.status_line.set("Loaded state".to_string());
                 }
             }
         }
         unimplemented!()
     }
 
-    pub fn input(&mut self, ev: InputEvent) {
-        unimplemented!()
+    pub fn input(&mut self, ev: InputEvent) -> InputResult {
+        let gamepad = &mut self.cpu.mem.input.gamepad_0;
+
+        match ev.event_type {
+            EventType::Right => gamepad.right = ev.active,
+            EventType::Down => gamepad.down = ev.active,
+            EventType::Left => gamepad.left = ev.active,
+            EventType::Up => gamepad.up = ev.active,
+            EventType::A => gamepad.a = ev.active,
+            EventType::B => gamepad.b = ev.active,
+            EventType::Start => gamepad.start = ev.active,
+            EventType::Select => gamepad.select = ev.active,
+            EventType::Quit => return InputResult::Quit,
+            EventType::Save => return InputResult::SaveState,
+            EventType::Load => return InputResult::LoadState
+        }
+
+        InputResult::Continue
     }
 }
+
+fn record_fps(last_time: &mut f64, frames: &mut usize) {
+    if cfg!(debug) {
+        let now = time::precise_time_s();
+        if now >= *last_time + 1f64 {
+            println!("{} FPS", *frames);
+            *frames = 0;
+            *last_time = now;
+        } else {
+            *frames += 1;
+        }
+    }
+}
+
